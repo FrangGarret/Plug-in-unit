e643dfe495c0d73ba2dcc5b25af614190f3017de
fangjialiang258351353 <18175361553@163.com>
2017-12-21 17:11:29 +0800

    12-21

--- fwd规范/js/common.js
+++ fwd规范/js/common.js
@@ -1,5 +1,7 @@
 //封装ajax方法
 // ajax封装
+//方家亮
+//2017-12-20
 function ajax(url, data, success, cache, alone, async, type, dataType, error) {
 	var type = type || 'post'; //请求类型
 	var dataType = dataType || 'json'; //接收数据类型
@@ -66,8 +68,8 @@ function ajax(url, data, success, cache, alone, async, type, dataType, error) {
 	});
 }
 
-
 // ajax提交(post方式提交)
+
 function post(url, data, success, cache, alone) {
 	ajax(url, data, success, cache, alone, false, 'post', 'json');
 }
@@ -81,9 +83,220 @@ function get(url, success, cache, alone) {
 function jsonp(url, success, cache, alone) {
 	ajax(url, {}, success, cache, alone, false, 'get', 'jsonp');
 }
-
+//---------------------------------------------------------------------------------------------------------------------
 //JSON数据拼接方法
-
+//方家亮
+//2017-12-21
+function createJson(prop, val, jsons) {
+	// 如果 val 被忽略
+	if(typeof val === "undefined") {
+		// 删除属性
+		delete jsons[prop];
+	} else {
+		// 添加 或 修改
+		jsons[prop] = val;
+	}
+}
+//---------------------------------------------------------------------------------------------------------------------
 //alert方法
+//方家亮
+//2017-12-21
+function fwd_alert(message, callback) {
+	var create = function() {
+		return document.createElement.apply(document, arguments);
+	}
 
+	var bgDialog = create("div"), //创建遮罩背景
+			look = create("p"),
+		dialog = create("div"), //创建当前弹出框
+		messageBox = create("ul"),
+		totalButton = create("div"),
+		okBubbton = create("button")
+	
+	dialog.className = "FWD_alert";
+	look.className = 'FWD_alert_tips'
+	bgDialog.className = "FWD_background";
+	messageBox.className = "FWD_alert_info_Warp";
+	totalButton.className = 'FWD_alert_btns_warp';
+	okBubbton.className = 'enter_btn';
+	
+	look.innerHTML = '温馨提示'
+	
+	if(typeof message == 'string') {
+		var messageLi = create("li");
+		messageLi.innerHTML = message.replace(/\[|]/g, '');
+		messageLi.className = "FWD_alert_info_one";
+		messageBox.appendChild(messageLi);
+	} else {
+		var messageJson = JSON.parse(message)
+		for(var i = 0; i < messageJson.length; i++) {
+			var messageLi = create("li");
+			messageLi.className = 'FWD_alert_info_more'
+			messageLi.innerHTML = messageJson[i];
+			messageBox.appendChild(messageLi);
+		}
+	}
+	okBubbton.innerHTML = "确定";
+	okBubbton.onclick = function() {
+
+		if(callback) {
+			callback(true);
+		}
+
+		bgDialog.parentNode.removeChild(bgDialog);
+
+	};
+	totalButton.appendChild(okBubbton);
+	dialog.appendChild(look);
+	dialog.appendChild(messageBox);
+	dialog.appendChild(totalButton);
+	bgDialog.appendChild(dialog);
+
+	document.body.appendChild(bgDialog);
+
+}
+//---------------------------------------------------------------------------------------------------------------------
+//自定义时间消失的弹出框
+function fwd_alert_diyTime(message,times,callback) {
+	var create = function() {
+		return document.createElement.apply(document, arguments);
+	}
+
+	var bgDialog = create("div"), //创建遮罩背景
+			look = create("p"),
+		dialog = create("div"), //创建当前弹出框
+		messageBox = create("ul")
+		
+	
+	dialog.className = "FWD_alert";
+	look.className = 'FWD_alert_tips'
+	bgDialog.className = "FWD_background";
+	messageBox.className = "FWD_alert_info_Warp";
+	look.innerHTML = '温馨提示'
+	
+	if(typeof message == 'string') {
+		var messageLi = create("li");
+		messageLi.innerHTML = message.replace(/\[|]/g, '');
+		messageLi.className = "FWD_alert_info_one";
+		messageBox.appendChild(messageLi);
+	} else {
+		var messageJson = JSON.parse(message)
+		for(var i = 0; i < messageJson.length; i++) {
+			var messageLi = create("li");
+			messageLi.className = 'FWD_alert_info_more'
+			messageLi.innerHTML = messageJson[i];
+			messageBox.appendChild(messageLi);
+		}
+	}
+
+	if(times){
+		setTimeout(function(){
+			if(callback){
+				callback(true);
+			}
+			bgDialog.parentNode.removeChild(bgDialog);
+		},times)
+	}else{
+			setTimeout(function(){
+				if(callback){
+					callback(true);
+				}
+				bgDialog.parentNode.removeChild(bgDialog);
+			},2000)
+			
+	}
+
+	
+	dialog.appendChild(look);
+	dialog.appendChild(messageBox);
+	
+	bgDialog.appendChild(dialog);
+
+	document.body.appendChild(bgDialog);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
 //confirm方法
+//方家亮
+//2017-12-21
+function FWD_confirm(message, callback) {
+
+	var create = function() {
+
+		return document.createElement.apply(document, arguments);
+
+	}
+
+	var bgDialog = create("div"), //创建半透明背景层
+		dialog = create("div"), //创建弹出框
+		//提示信息栏
+		look = create("p"),
+
+		messageBox = create("ul"),
+
+		totalButton = create("div"),
+
+		okBubbton = create("button"),
+
+		cancelButton = create("button");
+
+	dialog.className = "FWD_confirm";
+	look.className = "FWD_confirm_tips";
+
+	//添加背景层样式
+	bgDialog.className = "FWD_background";
+	messageBox.className = "FWD_confirm_info_Warp";
+	okBubbton.className = "enter_btn";
+	cancelButton.className = "cancel_btn";
+	totalButton.className = "FWD_confirm_btns_warp";
+
+	look.innerHTML = "温馨提示";
+	if(typeof message == 'string') {
+		var messageLi = create("li");
+		messageLi.className = 'FWD_confirm_info_one'
+		messageLi.innerHTML = message.replace(/\[|]/g, '');
+		messageLi.className = "alignCenter";
+		messageBox.appendChild(messageLi);
+	} else {
+		var messageJson = JSON.parse(message)
+		for(var i = 0; i < messageJson.length; i++) {
+			var messageLi = create("li");
+			messageLi.className = 'FWD_confirm_info_more'
+			messageLi.innerHTML = messageJson[i];
+			messageBox.appendChild(messageLi);
+		}
+	}
+
+	okBubbton.innerHTML = "確定";
+
+	cancelButton.innerHTML = "取消";
+
+	okBubbton.onclick = function() {
+		if(callback) {
+			callback(true);
+		}
+		bgDialog.parentNode.removeChild(bgDialog);
+
+	};
+
+	cancelButton.onclick = function() {
+		if(callback) {
+			callback(false);
+		}
+		bgDialog.parentNode.removeChild(bgDialog);
+	}
+
+	dialog.appendChild(look);
+	dialog.appendChild(messageBox);
+	dialog.appendChild(totalButton);
+	bgDialog.appendChild(dialog);
+	totalButton.appendChild(okBubbton);
+	totalButton.appendChild(cancelButton);
+	document.body.appendChild(bgDialog);
+
+}
+//---------------------------------------------------------------------------------------------------------------------
